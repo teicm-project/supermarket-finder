0c06b38c4c08ac33473bc98e78f62a5dacc12655
Iwanna Pantoula <iwanna.pantoyla@gmail.com>
2016-10-19 15:58:54 +0300

    Δημιουργία Βάσης
    Δημιουργία βάσης και καταχώρηση δεδομένων.

---
+++ src/main/java/teicm_team/supermarket_finder/AboutActivity.java
@@ -0,0 +1,28 @@
+package teicm_team.supermarket_finder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Δημιουργία Back Button στο toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    ///// Όταν πατάς το Back Button πάει στην MainActivity /////
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivityForResult(myIntent, 0);
+        return true;
+    }
+}
